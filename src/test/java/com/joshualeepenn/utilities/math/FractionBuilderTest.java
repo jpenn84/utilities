04d357bfd6f103aad36a42fbb0520c5796dcae78
Joshua Penn <joshualeepenn@gmail.com>
2024-10-07 09:03:33 -0400

    #3 add test coverage and bump version

--- src/test/java/com/joshualeepenn/utilities/math/FractionBuilderTest.java
+++ src/test/java/com/joshualeepenn/utilities/math/FractionBuilderTest.java
@@ -6,7 +6,14 @@ import org.testng.annotations.Test;
 public class FractionBuilderTest {
 
     @Test
+    public void testStaticFractionBuilder() {
+        Assert.assertEquals(FractionBuilder.getFraction(12345,67890), "¹²³⁴⁵/₆₇₈₉₀");
+    }
+
+    @Test
     public void testFractionBuilder() {
-        Assert.assertEquals(FractionBuilder.getFraction(12,34), "¹²/₃₄");
+        FractionBuilder fb = new FractionBuilder(67890, 12345);
+
+        Assert.assertEquals(fb.getFraction(), "⁶⁷⁸⁹⁰/₁₂₃₄₅");
     }
 }
